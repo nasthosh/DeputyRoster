90e20a39b1b9c252be7755ca080d028d77a88fbd
nasthosh <santhosh.karthik@outlook.com>
2020-10-07 20:07:51 +1100

    Initial commit

---
+++ src/Roster.java
@@ -0,0 +1,12 @@
+import java.util.*;
+import java.lang.*;
+
+public class Roster {
+    private int id;
+    private int employee;
+    private int department;
+    private Date startTime;
+    private Date endTime;
+    
+
+}
